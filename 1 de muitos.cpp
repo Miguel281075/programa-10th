05e60143b6d2bcb8d941b1d03766c57ab714c171
Miguel281075 <miguelvaladabotelho@gmail.com>
2025-09-29 11:43:33 +0100

    Add files via upload

---
+++ 1 de muitos.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Sua ,mae e tao gorda que tira selphie pelo google earth";
+
+    return 0;
+}
